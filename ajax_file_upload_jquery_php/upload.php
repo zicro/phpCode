d39795d9b1be0fea5e9744d96bd8deca830e8085
Omae <larhnimikit@gmail.com>
2018-01-28 21:21:23 +0000

    uploads

---
+++ ajax_file_upload_jquery_php/upload.php
@@ -0,0 +1,30 @@
+<?php
+if(isset($_POST) == true){
+	print_r($_POST);
+    //generate unique file name
+    $fileName = time().'_'.basename($_FILES["file"]["name"]);
+    
+    //file upload path
+    $targetDir = "uploads/";
+    $targetFilePath = $targetDir . $fileName;
+    
+    //allow certain file formats
+    $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+    $allowTypes = array('jpg','png','jpeg','gif');
+    
+    if(in_array($fileType, $allowTypes)){
+        //upload file to server
+        if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+            //insert file data into the database if needed
+            //........
+            $response['status'] = 'ok';
+        }else{
+            $response['status'] = 'err';
+        }
+    }else{
+        $response['status'] = 'type_err';
+    }
+    
+    //render response data in JSON format
+    echo json_encode($response);
+}
---
+++ drag-drop-file-upload-dropzone-php/ReadMe.txt
@@ -0,0 +1,16 @@
+Author: CodexWorld
+Author URL: http://www.codexworld.com/
+Author Email: contact@codexworld.com
+Tutorial Link: http://www.codexworld.com/drag-and-drop-file-upload-using-dropzone-php/
+
+============ Introduction ============
+This small project helps web developers to implement drag and drop files upload in PHP.
+
+============ Installation ============
+1. Create a database (codexworld) at phpMyAdmin.
+2. Import the "files.sql" file into the database (codexworld).
+3. Open the "upload.php" file and modify the $dbHost, $dbUsername, $dbPassword and $dbName variables value with your MySQL details.
+4. Run the "index.html" file at the browser and test the functionalities.
+
+============ May I Help You ===========
+If you have any query about this script, please feel free to comment here - http://www.codexworld.com/drag-and-drop-file-upload-using-dropzone-php/#respond. We will reply your query ASAP.
